8e130f13df9a66217eac5f2e7a5fc1af26660430
Gabe Jackson <gj@mail.co.de>
2017-09-25 17:33:19 -0400

    Fix typos; update lifecycle hooks; use anonymous functions instead of arrow functions since they won't look quite so foreign to students looking at the test suite

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,26 +1,28 @@
 const expect = chai.expect;
 
-describe('index.js', () => {
-  describe('driver', () => {
-    it('sets a variable driver equal to bob', () => {
-      expect(driver).to.equal('bob')
-    })
-  })
+describe('index.js', function () {
+  describe('driver', function () {
+    it('sets a variable driver equal to bob', function () {
+      expect(driver).to.equal('bob');
+    });
+  });
 
-  describe('console.log()', () => {
-    let spy;
+  describe('console.log()', function () {
+    const spy = sinon.spy(console, 'log');
 
-    spy = sinon.spy(console, 'log');
+    after(function () {
+      spy.restore();
+    });
 
-    it('invokes console.log() with the string "this code was called"', () => {
+    it('invokes console.log() with the string "this code was called"', function () {
       expect(spy.calledWithExactly('this code was called')).to.be.true;
     });
 
-    it('invokes console.log() with the `driver` variable', () => {
+    it('invokes console.log() with the `driver` variable', function () {
       expect(spy.calledWithExactly('bob')).to.be.true;
     });
 
-    it('invokes the console.log with the `driver` variable as the first argumnent and `is the driver variable name` as the second argument', () => {
+    it('invokes the console.log with the `driver` variable as the first argument and `is the driver variable name` as the second argument', function () {
       expect(spy.calledWithExactly('bob', 'is the driver variable name')).to.be.true;
     });
   });
